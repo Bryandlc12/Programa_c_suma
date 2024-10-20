058053988a17dd6906e005a25bb548a796a7df37
Bryandlc12 <160089099+Bryandlc12@users.noreply.github.com>
2024-10-20 11:10:34 -0600

    funcion_suma.c

--- funcion_suma.c
+++ funcion_suma.c
@@ -4,3 +4,16 @@ struct valores {
     int a;
     int b;
 };
+int main (void) {
+    struct valores val1;
+    struct valores val2;
+    val1.a = 3;
+    val1.b = 4;
+    val2.a = 6;
+    val2.b = 8;
+
+    suma (val1, val2);
+    resta (val1, val2);
+    divicion(val1, val2);
+
+}
